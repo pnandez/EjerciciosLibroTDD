a381c0fab9776315bd81243bf10d51eb28b3f195
pnandez <paquiherca@gmail.com>
2022-04-12 11:33:29 +0100

    make cif and nif fields exclusive in green

--- src/csvFilter.ts
+++ src/csvFilter.ts
@@ -6,16 +6,26 @@ export class CsvFilter {
     const fields = invoice.split(',');
     const ivaFieldIndex = 4;
     const igicFieldIndex = 5;
+    const cifFieldIndex = 7;
+    const nifFieldIndex = 8;
     const ivaField = fields[ivaFieldIndex];
     const igicField = fields[igicFieldIndex];
+    const cifField = fields[cifFieldIndex];
+    const nifField = fields[nifFieldIndex];
     const decimalRegex = new RegExp('\\d+(\\.\\d+)?');
     const taxFieldsAreMutuallyExclusive =
       (ivaField.match(decimalRegex) || igicField.match(decimalRegex)) &&
-      (ivaField === null || ivaField.length === 0 ||
-      igicField === null || igicField.length === 0);
+      (this.isNullOrEmpty(ivaField) || this.isNullOrEmpty(igicField));
     if (taxFieldsAreMutuallyExclusive) {
-      result.push(lines[1]);
+      if ((!this.isNullOrEmpty(cifField) && this.isNullOrEmpty(nifField)) ||
+        (this.isNullOrEmpty(cifField) && !this.isNullOrEmpty(nifField))) {
+        result.push(lines[1]);
+      }
     }
     return result;
+  }
+
+  private isNullOrEmpty(valueToCheck:string): boolean {
+    return (valueToCheck === null || valueToCheck.length === 0);
   }
 }
--- test/csvFilter.spec.ts
+++ test/csvFilter.spec.ts
@@ -24,10 +24,10 @@ const fileWithOneInvoiceLineHaving = (cif: string = 'B76430134', nif: string = e
 
 /**
  * Tras analizarlo con los especialistas en el negocio las reglas son:
- *    // Es válido que algunos campos estén vacíos (apareciendo dos comas seguidas o una coma final)
+ *   // Es válido que algunos campos estén vacíos (apareciendo dos comas seguidas o una coma final)
  *    El número de factura no puede estar repetido. Si lo estuviese eliminaríamos todas las líneas con repetición.
- *    //Los impuestos IVA e IGIC son excluyentes, sólo puede aplicarse uno de los dos. Si alguna línea tiene contenido en ambos campos debe quedarse fuera.
- *    Los campos CIF y NIF son excluyentes, sólo se puede usar uno de ellos.
+ *   // Los impuestos IVA e IGIC son excluyentes, sólo puede aplicarse uno de los dos. Si alguna línea tiene contenido en ambos campos debe quedarse fuera.
+ *   // Los campos CIF y NIF son excluyentes, sólo se puede usar uno de ellos.
  *    El neto es el resultado de aplicar al bruto el correspondiente impuesto. Si algún neto no está bien calculado se queda fuera.
  *    Un fichero con una sola factura donde todo es correcto, debería producir como salida la misma línea
  *    Un fichero con una sola factura donde IVA e IGIC están rellenos, debería eliminar la línea
