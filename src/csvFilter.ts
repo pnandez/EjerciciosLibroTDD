799102ca82751cec900d60854280ae5d5521c5cf
pnandez <paquiherca@gmail.com>
2022-04-12 11:46:24 +0100

    Refactor extract variable + extract methods

--- src/csvFilter.ts
+++ src/csvFilter.ts
@@ -1,28 +1,48 @@
+const indexDictionary = {
+  'invoiceId': 0,
+  'invoiceDate': 1,
+  'grossAmount': 2,
+  'netAmount': 3,
+  'ivaFieldIndex': 4,
+  'igicFieldIndex': 5,
+  'cifFieldIndex': 7,
+  'nifFieldIndex': 8,
+};
+
 export class CsvFilter {
   public apply(lines: string[]): string[] {
     const result: string[] = [];
     result.push(lines[0]);
     const invoice = lines[1];
     const fields = invoice.split(',');
-    const ivaFieldIndex = 4;
-    const igicFieldIndex = 5;
-    const cifFieldIndex = 7;
-    const nifFieldIndex = 8;
-    const ivaField = fields[ivaFieldIndex];
-    const igicField = fields[igicFieldIndex];
-    const cifField = fields[cifFieldIndex];
-    const nifField = fields[nifFieldIndex];
+
+    if (this.checkTaxFields(fields) && this.checkIdentificationFields(fields)) {
+      result.push(lines[1]);
+    }
+    return result;
+  }
+
+  private checkTaxFields(fields: string[]):boolean {
+    const ivaField = fields[indexDictionary.ivaFieldIndex];
+    const igicField = fields[indexDictionary.igicFieldIndex];
     const decimalRegex = new RegExp('\\d+(\\.\\d+)?');
     const taxFieldsAreMutuallyExclusive =
       (ivaField.match(decimalRegex) || igicField.match(decimalRegex)) &&
       (this.isNullOrEmpty(ivaField) || this.isNullOrEmpty(igicField));
+
+    if (taxFieldsAreMutuallyExclusive) return true;
+    return false;
+  }
+
+  private checkIdentificationFields(fields: string[]): boolean {
+    const cifField = fields[indexDictionary.cifFieldIndex];
+    const nifField = fields[indexDictionary.nifFieldIndex];
     const identificationFieldsAreMutuallyExclusive =
       (!this.isNullOrEmpty(cifField) && this.isNullOrEmpty(nifField) ||
         (this.isNullOrEmpty(cifField) && !this.isNullOrEmpty(nifField)));
-    if (taxFieldsAreMutuallyExclusive && identificationFieldsAreMutuallyExclusive) {
-      result.push(lines[1]);
-    }
-    return result;
+
+    if (identificationFieldsAreMutuallyExclusive) return true;
+    return false;
   }
 
   private isNullOrEmpty(valueToCheck:string): boolean {
